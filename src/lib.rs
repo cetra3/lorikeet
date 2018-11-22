9ba2fc322d664989e46958f303144508fb711cb3
Ashkan Kiani <ashkan@zephyr>
2018-11-22 06:06:31 -0800

    Update reqwest to 0.9.5 and rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,11 @@ extern crate petgraph;
 
 #[macro_use]
 extern crate serde_derive;
-extern crate serde_yaml;
 extern crate serde;
+extern crate serde_yaml;
 
-#[macro_use] extern crate log;
+#[macro_use]
+extern crate log;
 
 #[macro_use]
 extern crate lazy_static;
@@ -17,8 +18,8 @@ extern crate linked_hash_map;
 
 extern crate regex;
 
-extern crate reqwest;
 extern crate hyper;
+extern crate reqwest;
 
 extern crate term_painter;
 
@@ -36,9 +37,9 @@ extern crate failure;
 
 extern crate quick_xml;
 
-pub mod runner;
-pub mod yaml;
-pub mod step;
 pub mod graph;
-pub mod submitter;
 pub mod junit;
+pub mod runner;
+pub mod step;
+pub mod submitter;
+pub mod yaml;
--- src/submitter.rs
+++ src/submitter.rs
@@ -16,23 +16,26 @@ pub struct StepResult {
     pub pass: bool,
     pub output: String,
     pub error: Option<String>,
-    pub duration: f32
+    pub duration: f32,
 }
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct WebHook {
     hostname: String,
     has_errors: bool,
-    tests: Vec<StepResult>
+    tests: Vec<StepResult>,
 }
 
-pub fn submit_webhook(results: &Vec<StepResult>, url: &str, hostname: Option<&str>) -> Result<(),reqwest::Error> {
-
+pub fn submit_webhook(
+    results: &Vec<StepResult>,
+    url: &str,
+    hostname: Option<&str>,
+) -> Result<(), reqwest::Error> {
     debug!("Submitting webhook to:{}", url);
 
     let hostname = match hostname {
         Some(hostname) => String::from(hostname),
-        None => hostname::get_hostname().unwrap_or_else(||String::from(""))
+        None => hostname::get_hostname().unwrap_or_else(|| String::from("")),
     };
 
     debug!("Hostname is:{}", hostname);
@@ -44,24 +47,23 @@ pub fn submit_webhook(results: &Vec<StepResult>, url: &str, hostname: Option<&st
     let payload = WebHook {
         hostname: hostname,
         has_errors: has_errors,
-        tests: results.clone()
+        tests: results.clone(),
     };
 
     debug!("WebHook payload is:{:?}", payload);
 
     let client = reqwest::Client::new();
 
-    let mut builder = client.post(url);
+    let builder = client.post(url);
 
     debug!("Create Reqwest Client");
 
-    builder.json(&payload);
+    let builder = builder.json(&payload);
 
     debug!("Appended JSON payload");
 
-    let result = 
-        builder.send()?;
-    
+    let result = builder.send()?;
+
     debug!("Sent request! Result is {:?}", result);
 
     Ok(())
@@ -69,10 +71,9 @@ pub fn submit_webhook(results: &Vec<StepResult>, url: &str, hostname: Option<&st
 
 impl StepResult {
     pub fn terminal_print(&self, colours: &bool) {
-
         let style = match self.pass {
             true => Green.bold(),
-            false => Red.bold()
+            false => Red.bold(),
         };
 
         let mut message = format!("- name: {}\n", self.name);
@@ -84,9 +85,11 @@ impl StepResult {
         message.push_str(&format!("  pass: {}\n", self.pass));
 
         if self.output != "" {
-
             if self.output.contains("\n") {
-                message.push_str(&format!("  output: |\n    {}\n", self.output.replace("\n", "\n    ")));
+                message.push_str(&format!(
+                    "  output: |\n    {}\n",
+                    self.output.replace("\n", "\n    ")
+                ));
             } else {
                 message.push_str(&format!("  output: {}\n", self.output));
             }
@@ -103,28 +106,25 @@ impl StepResult {
         } else {
             println!("{}", message);
         }
-
     }
 }
 
 impl From<Step> for StepResult {
     fn from(step: Step) -> Self {
-
         let duration = step.get_duration_ms();
         let name = step.name;
         let description = step.description;
 
         let (pass, output, error) = match step.outcome {
             Some(outcome) => {
-
                 let output = match step.do_output {
                     true => outcome.output.unwrap_or_default(),
-                    false => String::new()
+                    false => String::new(),
                 };
 
                 (outcome.error.is_none(), output, outcome.error)
-            },
-            None => (false, String::new(), Some(String::from("Not finished")))
+            }
+            None => (false, String::new(), Some(String::from("Not finished"))),
         };
 
         StepResult {
@@ -133,8 +133,7 @@ impl From<Step> for StepResult {
             description,
             pass,
             output,
-            error
+            error,
         }
     }
-
 }
