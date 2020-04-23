7b27e74f7f642e17826a3aac2a42aed14e6fc0c6
cetra3 <cetra3@hotmail.com>
2020-04-23 13:10:11 +0930

    Adjust runner logic to not use Mutex anymore

--- src/runner.rs
+++ src/runner.rs
@@ -1,9 +1,7 @@
 use crate::step::FilterType;
 
-use std::sync::Arc;
 use std::time::Duration;
 use tokio::sync::mpsc::{unbounded_channel, UnboundedSender};
-use tokio::sync::Mutex;
 
 use crate::step::{ExpectType, Outcome, RetryPolicy, RunType, Step, STEP_OUTPUT};
 
@@ -11,127 +9,58 @@ use crate::graph::{create_graph, Require};
 use petgraph::prelude::GraphMap;
 use petgraph::{Directed, Direction};
 
-use serde::Deserialize;
+use log::*;
 
-use log::debug;
+use anyhow::Error;
 
-use anyhow::{anyhow, Error};
-
-use chashmap::CHashMap;
-
-pub struct StepRunner<'a> {
+pub struct StepRunner {
+    pub name: String,
+    pub index: usize,
     pub run: RunType,
     pub expect: ExpectType,
     pub retry: RetryPolicy,
     pub filters: Vec<FilterType>,
-    pub graph: Arc<GraphMap<usize, Require, Directed>>,
-    pub steps: Arc<Mutex<Vec<Status>>>,
-    pub name: &'a str,
-    pub name_lookup: &'a CHashMap<&'a str, usize>,
-    pub index: usize,
-    pub notify: UnboundedSender<usize>,
+    pub notify: UnboundedSender<(usize, Outcome)>,
 }
 
-#[derive(Clone, Debug, PartialEq, Deserialize)]
-pub enum Status {
-    InProgress,
-    Outstanding,
-    Completed(Outcome),
-}
+//Spawns into a background task so we can poll the rest
+impl StepRunner {
+    pub async fn poll(self) {
+        debug!("Running: {}", self.name);
 
-impl<'a> StepRunner<'a> {
-    pub async fn poll(&self) {
-        debug!("Poll received for `{}`", self.index);
+        tokio::spawn(async move {
+            let outcome = self
+                .run
+                .execute(self.expect, self.filters, self.retry)
+                .await;
 
-        let mut cur_steps = self.steps.lock().await;
-
-        match cur_steps[self.index] {
-            //If it's already completed, return
-            Status::Completed(_) => {
-                return;
+            if let Some(ref output) = outcome.output {
+                STEP_OUTPUT.insert(self.name.clone(), output.clone());
             }
-            _ => (),
-        }
 
-        let mut has_error = false;
+            if let Err(err) = self.notify.send((self.index, outcome)) {
+                error!("Could not notify executor:{}", err);
+            }
 
-        for neighbor in self
-            .graph
-            .neighbors_directed(self.index, Direction::Incoming)
-        {
-            match cur_steps[neighbor] {
-                Status::Completed(ref status_outcome) => {
-                    if let Some(_) = status_outcome.error {
-                        self.notify
-                            .send(self.index)
-                            .expect("Could not notify executor");
-                        has_error = true;
-                        break;
-                    }
-                }
-                _ => {
-                    debug!(
-                        "Neighbor {} isn't completed for {}, skipping",
-                        neighbor, self.index
-                    );
-                    return;
-                }
-            };
-        }
-
-        if has_error {
-            cur_steps[self.index] = Status::Completed(Outcome {
-                output: Some("".into()),
-                error: Some("Dependency Not Met".into()),
-                duration: Duration::from_secs(0),
-            });
-            return;
-        }
-
-        if cur_steps[self.index] == Status::Outstanding {
-            cur_steps[self.index] = Status::InProgress;
-
-            let run = self.run.clone();
-
-            let expect = self.expect.clone();
-            let retry = self.retry;
-            let tx = self.notify.clone();
-            let index = self.index;
-            let steps = self.steps.clone();
-            let filters = self.filters.clone();
-            let name = self.name.to_string();
-
-            //let task = task::current();
-            tokio::spawn(async move {
-                let outcome = run.execute(expect, filters, retry).await;
-                debug!("Step `{}` done: {:?}", index, outcome);
-                if let Some(ref output) = outcome.output {
-                    STEP_OUTPUT.insert(name, output.clone());
-                }
-
-                steps.lock().await[index] = Status::Completed(outcome);
-                tx.send(index).expect("Could not notify executor");
-            });
-        }
+            debug!("Completed: {}", self.name);
+        });
     }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+enum Status {
+    Awaiting,
+    Completed(Outcome),
 }
 
 pub async fn run_steps(steps: &mut Vec<Step>) -> Result<(), Error> {
     let graph = create_graph(&steps)?;
 
-    let steps_status: Arc<Mutex<Vec<Status>>> =
-        Arc::new(Mutex::new(vec![Status::Outstanding; steps.len()]));
+    let mut statuses = Vec::new();
+    statuses.resize(steps.len(), Status::Awaiting);
 
     //We want the runners to drop after this so we can return the steps status
     {
-        let lookup: CHashMap<&str, usize> = CHashMap::new();
-
-        for i in 0..steps.len() {
-            lookup.insert(&steps[i].name, i);
-        }
-
-        let shared_graph = Arc::new(graph);
-
         let mut runners = Vec::new();
 
         let (tx, mut rx) = unbounded_channel();
@@ -142,39 +71,83 @@ pub async fn run_steps(steps: &mut Vec<Step>) -> Result<(), Error> {
                 expect: steps[i].expect.clone(),
                 retry: steps[i].retry,
                 filters: steps[i].filters.clone(),
-                name: &steps[i].name,
-                graph: shared_graph.clone(),
-                steps: steps_status.clone(),
+                name: steps[i].name.clone(),
                 index: i,
-                name_lookup: &lookup,
                 notify: tx.clone(),
             };
 
             runners.push(future);
         }
 
-        //Kick off the process
-        for runner in runners.iter_mut() {
+        //We want to start all the ones that don't have any outgoing neighbors
+        let (to_start, waiting) = runners
+            .into_iter()
+            .partition::<Vec<StepRunner>, _>(|job| can_start(job.index, &statuses, &graph));
+
+        runners = waiting;
+
+        let mut active = 0;
+
+        for runner in to_start.into_iter() {
             runner.poll().await;
+            active += 1;
         }
 
-        for _ in 0..steps.len() {
-            if let Some(finished) = rx.recv().await {
-                for neighbor in shared_graph.neighbors_directed(finished, Direction::Outgoing) {
-                    runners[neighbor].poll().await;
+        while active > 0 {
+            debug!(
+                "Active amount: {}, runners waiting: {}",
+                active,
+                runners.len()
+            );
+            if let Some((idx, outcome)) = rx.recv().await {
+                active -= 1;
+                let has_error = outcome.error.is_some();
+
+                statuses[idx] = Status::Completed(outcome);
+
+                for neighbor in graph.neighbors_directed(idx, Direction::Outgoing) {
+                    if let Some(job_idx) = runners.iter().position(|job| job.index == neighbor) {
+                        if !has_error && can_start(runners[job_idx].index, &statuses, &graph) {
+                            let runner = runners.swap_remove(job_idx);
+                            runner.poll().await;
+                            active += 1;
+                        }
+                    }
                 }
             }
         }
     }
 
-    let steps_ptr =
-        Arc::try_unwrap(steps_status).map_err(|_| anyhow!("Could not unwrap arc pointer"))?;
-
-    for (i, status) in steps_ptr.into_inner().into_iter().enumerate() {
+    for (i, status) in statuses.into_iter().enumerate() {
         if let Status::Completed(outcome) = status {
             steps[i].outcome = Some(outcome);
+        } else {
+            steps[i].outcome = Some(Outcome {
+                output: Some("".into()),
+                error: Some("Dependency Not Met".into()),
+                duration: Duration::from_secs(0),
+            });
         }
     }
 
     Ok(())
+}
+
+fn can_start(
+    idx: usize,
+    statuses: &Vec<Status>,
+    graph: &GraphMap<usize, Require, Directed>,
+) -> bool {
+    debug!("Checking if we can start for {}", idx);
+
+    for neighbor in graph.neighbors_directed(idx, Direction::Incoming) {
+        if statuses[neighbor] == Status::Awaiting {
+            debug!("Neighbour {} Not Completed", neighbor);
+            return false;
+        } else {
+            debug!("Neighbour {} Completed", neighbor);
+        }
+    }
+
+    return true;
 }
